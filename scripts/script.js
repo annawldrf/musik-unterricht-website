224ab6761043ba345d01c020e79dc1489bebf956
paula garcia <paula.garciadv@gmail.com>
2021-12-15 16:20:04 +0100

    navbar style

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
